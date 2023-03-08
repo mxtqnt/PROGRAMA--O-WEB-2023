351ab1ff324907132141bc21bb04055fddcbbf72
Mxtqnt <anacarolinass.cc@gmail.com>
2023-03-08 10:19:32 -0300

    first commit

---
+++ python/Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.8-alpine
+
+RUN apk add --update \
+    rust cargo build-base libffi-dev openssl-dev \
+    xmlsec xmlsec-dev \
+  && rm -rf /var/cache/apk/*
+
+ADD requirements.txt /tmp
+RUN pip install -r /tmp/requirements.txt
+
+EXPOSE 8000
+CMD python index.py 
---
+++ python/index.py
@@ -0,0 +1,20 @@
+def app(environ, start_response):
+
+    data = b"Hello, Web!\n"
+
+    start_response("200 OK", [
+
+        ("Content-Type", "text/plain"),
+
+        ("Content-Length", str(len(data)))
+
+    ])
+
+    return iter([data])
+
+if __name__ == '__main__':
+
+    from gevent.pywsgi import WSGIServer
+
+    WSGIServer(('', 8000),app, spawn=None).serve_forever()
+    
