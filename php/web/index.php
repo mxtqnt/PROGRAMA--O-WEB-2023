b3036d04fb8d847b7b76eaa7b916fe0c8de32c67
Mxtqnt <anacarolinass.cc@gmail.com>
2023-03-14 11:36:42 -0300

    Aula 13/03

--- php/web/index.php
+++ php/web/index.php
@@ -1,3 +1,20 @@
 <?php
-
-echo "Hello World!";
+$data ="";
+$forms_data = [];
+if ($_SERVER['REQUEST_URI'] === '/')
+	$data="Hello World";
+if ($_SERVER['REQUEST_URI'] === '/feedback') {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['feedback'];
+        $feedback = [
+            'name' => $name,
+            'email' => $email,
+            'feedback' => $message,
+        ];
+        array_push($forms_data, $feedback);
+        $data = b"Your feedback submitted successfully.";
+    }
+}
+echo $data;
--- python/index.py
+++ python/index.py
@@ -1,20 +1,37 @@
-def app(environ, start_response):
+import wsgiref.simple_server
+import urllib.parse
 
-    data = b"Oi namorada linda!\n"
+def app(environ, start_response):
+    path = environ["PATH_INFO"]
+    method = environ["REQUEST_METHOD"]
+    data=b""
+    forms_data = []
+    if path == "/":
+        data = b"Hello, Web!\n"
+    if path == "/feedback":
+        if method == "POST":
+            input_obj = environ["wsgi.input"]
+            input_length = int(environ["CONTENT_LENGTH"])
+            body = input_obj.read(input_length).decode('utf-8')
+            params = urllib.parse.parse_qs(body, keep_blank_values=True)
+            req = { 
+                "name" : params.get('name', [''])[0],
+                "email" : params.get('email', [''])[0],
+                "message" : params.get('message', [''])[0]
+            }
+            forms_data.append(req)
+            data = b"Your feedback submitted successfully."
 
     start_response("200 OK", [
-
         ("Content-Type", "text/plain"),
-
         ("Content-Length", str(len(data)))
-
     ])
-
     return iter([data])
 
 if __name__ == '__main__':
-
-    from gevent.pywsgi import WSGIServer
-
-    WSGIServer(('', 8000),app, spawn=None).serve_forever()
-    
+    w_s = wsgiref.simple_server.make_server(
+        host="",
+        port=8000,
+        app=app
+    )
+    w_s.serve_forever()
